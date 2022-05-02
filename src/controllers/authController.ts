6bba6cbdc7e6086a7ee7dd3f8dddaa3499d88dd8
Jazza <joaoricardo.silver@gmail.com>
2022-05-01 23:06:56 -0300

    Autenticação por token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,93 +3,81 @@ import prismaClient from "../config/prismaConfig";
 import bcrypt from "bcrypt";
 import jwt, { Secret } from "jsonwebtoken";
 
-class AuthController {
-  public tokens: string[] = [];
+let refreshTokens: string[] = [];
 
-  async signIn(req: Request, res: Response) {
-    console.log(this.tokens);
-    const { email, password, username } = req.body;
+const generateAccessToken = (user: any) =>
+  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET as Secret, {
+    expiresIn: "10m",
+  });
 
-    try {
-      const user = await prismaClient.user.findFirst({
-        where: { username: { equals: username } },
-      });
+export async function signIn(req: Request, res: Response) {
+  const { password, username } = req.body;
 
-      const verification = await bcrypt.compare(password, user!.password);
+  try {
+    const user = await prismaClient.user.findFirst({
+      where: { username: { equals: username } },
+    });
 
-      if (!verification) {
-        console.log(user);
-        return res
-          .status(401)
-          .json({ message: "Usuário e/ou senha Incorretos" });
-      }
+    const verification = await bcrypt.compare(password, user!.password);
 
-      const accessToken = jwt.sign(
-        user as Record<string, any>,
-        process.env.ACCESS_TOKEN_SECRET as Secret,
-        { expiresIn: "5s" }
-      );
-
-      const refreshToken = jwt.sign(
-        user as Record<string, any>,
-        process.env.REFRESH_TOKEN_SECRET as Secret
-      );
-
-      this.tokens.push(accessToken);
-
-      return res
-        .status(200)
-        .json({ userData: user, token: accessToken, refreshToken });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: true, errordata: error });
+    if (!verification) {
+      console.log(user);
+      return res.status(401).json({ message: "Usuário e/ou senha Incorretos" });
     }
-  }
 
-  async registerIn(req: Request, res: Response) {
-    const { email, password, username } = req.body;
+    const accessToken = generateAccessToken(user);
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const refreshToken = jwt.sign(
+      user as Record<string, any>,
+      process.env.REFRESH_TOKEN_SECRET as Secret
+    );
+    refreshTokens.push(refreshToken);
 
-    try {
-      await prismaClient.user.create({
-        data: {
-          email,
-          password: hashedPassword,
-          username,
-        },
-      });
-      return res.sendStatus(201);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ error: true, errordata: error });
-    }
-  }
-
-  async getToken(req: Request, res: Response) {
-    const refreshToken = req.body.token;
-    if (refreshToken == null) return res.sendStatus(401);
-    if (refreshToken.includes(refreshToken)) return res.sendStatus(403);
-    try {
-      const user = jwt.verify(
-        refreshToken,
-        process.env.REFRESH_TOKEN_SECRET as Secret
-      );
-
-      const generateAccessToken = jwt.sign(
-        user as Record<string, any>,
-        process.env.ACCESS_TOKEN_SECRET as Secret,
-        { expiresIn: "5s" }
-      );
-    } catch (error) {
-      console.log(error);
-      return res.sendStatus(403);
-    }
-  }
-
-  async logOut(req: Request, res: Response) {
-    return console.log("ss fon");
+    return res
+      .status(200)
+      .json({ userData: user, token: accessToken, refreshToken });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: true, errordata: error });
   }
 }
 
-export default AuthController;
+export async function registerIn(req: Request, res: Response) {
+  const { email, password, username } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await prismaClient.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        username,
+      },
+    });
+    return res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: true, errordata: error });
+  }
+}
+
+export async function getToken(req: Request, res: Response) {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(
+    refreshToken,
+    process.env.REFRESH_TOKEN_SECRET as Secret,
+    (error: any, user: any) => {
+      if (error) return res.sendStatus(407);
+      const accessToken = generateAccessToken({ name: user.name });
+      res.json({ accessToken: accessToken });
+    }
+  );
+}
+
+export async function logOut(req: Request, res: Response) {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,27 @@
 import express from "express";
 import ROUTES from "../constants/routes";
-import AuthController from "../controllers/authController";
 import TransactionsController from "../controllers/transactionsController";
 import VerifyToken from "../middleware/VerifyToken";
+import {
+  getToken,
+  logOut,
+  registerIn,
+  signIn,
+} from "../controllers/authController";
 
 const transactionsController = new TransactionsController();
 const router = express.Router();
-const authController = new AuthController();
 
-router.route(ROUTES.LOGIN).post(authController.signIn);
-router.route(ROUTES.LOGOUT).delete(authController.logOut);
-router.route(ROUTES.REGISTER).post(authController.registerIn);
-router.route(ROUTES.TOKEN).post(authController.getToken);
+router.route(ROUTES.LOGIN).post(signIn);
+router.route(ROUTES.LOGOUT).delete(logOut);
+router.route(ROUTES.REGISTER).post(registerIn);
+router.route(ROUTES.TOKEN).post(getToken);
 router
   .route(ROUTES.TRANSACTIONS)
-  .get(transactionsController.getAll)
-  .post(transactionsController.create)
-  .put(transactionsController.edit)
-  .delete(transactionsController.delete);
-router.route(ROUTES.EXTRATO).get(transactionsController.extrato);
+  .get(VerifyToken, transactionsController.getAll)
+  .post(VerifyToken, transactionsController.create)
+  .put(VerifyToken, transactionsController.edit)
+  .delete(VerifyToken, transactionsController.delete);
+router.route(ROUTES.EXTRATO).get(VerifyToken, transactionsController.extrato);
 
 export default router;
